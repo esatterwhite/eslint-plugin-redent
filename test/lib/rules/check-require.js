eec316e0741aad36f2b35d6c55d0c61047b75164
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-12-30 17:25:07 -0600

    fix(rules): corrects an invalid options lookup in check-require
    The eslint configs are and array of 1: level, 2: when condition and
    3: rule config. The level is popped off making the second index to place
    for configuration.
    
    This fixes the index lookup and updates tests to pass inline options to
    the rule with a custom package root location
    
    Semver: patch

--- test/lib/rules/check-require.js
+++ test/lib/rules/check-require.js
@@ -9,26 +9,49 @@ const rule = require('../../../lib/rules/check-require')
 const fixture_path = path.join(__dirname, '..', '..', 'fixture')
 const fixture = fs.readFileSync(fixture_path, 'utf8')
 
-const Suite = new RuleTester({
-  parserOptions: {
-    ecmaVersion: 2018
-  , sourceType: 'script'
-  }
-, rules: {
-    "check-require": 2
-  }
-})
+{
+  const Suite = new RuleTester({
+    parserOptions: {
+      ecmaVersion: 2018
+    , sourceType: 'script'
+    }
+  })
 
-Suite.run('check-require', rule, {
-  valid: [
-    {code: 'const a = require("http")'}
-  , {code: fixture}
-  , {code: 'var a = require("eslint/lib/rules/utils/ast-utils")'}
-  ]
-, invalid: [{
-    code: 'const thing = require("biscuits")'
-  , errors: [{
-      message: 'Missing dependency: "biscuits". Not listed in package.json'
+  Suite.run('check-require', rule, {
+    valid: [
+      {code: 'const a = require("http")'}
+    , {code: fixture}
+    , {code: 'var a = require("eslint/lib/rules/utils/ast-utils")'}
+    ]
+  , invalid: [{
+      code: 'const thing = require("biscuits")'
+    , errors: [{
+        message: 'Missing dependency: "biscuits". Not listed in package.json'
+      }]
     }]
-  }]
-})
+  })
+}
+
+{
+  const Suite = new RuleTester({
+    parserOptions: {
+      ecmaVersion: 2018
+    , sourceType: 'script'
+    }
+  })
+
+  Suite.run('check-require', rule, {
+    valid: [
+      {code: 'const a = require("http")'}
+    , {code: fixture}
+    , {code: 'const thing = require("biscuits")', options: ["always", {root: __dirname}]}
+    ]
+  , invalid: [{
+      code: 'var a = require("eslint/lib/rules/utils/ast-utils")'
+    , options: ["always", {root: __dirname}]
+    , errors: [{
+        message: 'Missing dependency: "eslint". Not listed in package.json'
+      }]
+    }]
+  })
+}
