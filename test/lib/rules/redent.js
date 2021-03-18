f4affe32035ad3fd55f367b7674807ed2d8e9f3a
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-03-17 21:18:15 -0500

    fix(indent): fix indent cache tracking
    There are places that track indents as strings and places that track
    them as numeric values. In the case of ignored tokens it would result in
    numeric values being added with string values `00010`. This makes sure
    the values in the indent cache are always numeric based on the indent
    size
    
    fixes: gh-9

--- test/lib/rules/redent.js
+++ test/lib/rules/redent.js
@@ -6,12 +6,16 @@ const fs = require('fs')
 const {RuleTester} = require('eslint')
 const rule = require('../../../lib/rules/indent')
 
-const fixture_path = path.join(__dirname, '..', '..', 'fixture')
-const fixture = fs.readFileSync(fixture_path, 'utf8')
+const fixture_path = path.join(__dirname, '..', '..', 'fixtures')
+const basic = fs.readFileSync(path.join(fixture_path, 'valid'), 'utf8')
+const inconsistent_whitespace = fs.readFileSync(
+  path.join(fixture_path, 'inconsistent-whitespace')
+, 'utf8'
+)
 
 const Suite = new RuleTester({
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2019
   , sourceType: 'module'
   }
 , rules: {
@@ -21,11 +25,13 @@ const Suite = new RuleTester({
 
 Suite.run('redent', rule, {
   valid: [
-    {code: 'var x = {\n  a: 1\n, b: 2\n}\nvar a = 1\n  + 5'}
-  , {code: fixture}
+ // {code: 'var x = {\n  a: 1\n, b: 2\n}\nvar a = 1\n  + 5'}
+//  , {code: basic}
+   {code: inconsistent_whitespace}
   ]
 , invalid: [{
     code: 'var x = {\n  a: 1\n  , b: 2\n}\n'
+  , output: 'var x = {\n  a: 1\n, b: 2\n}\n'
   , errors: [{
       message: 'Expected indentation of 0 spaces but found 2.'
     , type: 'Punctuator'
